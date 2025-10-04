eafae10a281421fb6d080d5390a0361092a33ac6
dhruva7vernekar-blip <dhruva7vernekar@gmail.com>
2025-10-04 11:10:39 +0530

    save

--- odd.py
+++ odd.py
@@ -1,15 +1,27 @@
-def count_even_odd(numbers):
-    even_count = 0
-    odd_count = 0
-    for num in numbers:
-        if num % 2 == 0:
-            even_count += 1
-        else:
-            odd_count += 1
-    return even_count, odd_count
+# Program to count even, odd, and prime numbers in a list
 
-# Example usage:
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-even, odd = count_even_odd(numbers)
-print(f"Even numbers: {even}")
-print(f"Odd numbers: {odd}")
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+numbers = list(map(int, input("Enter numbers separated by space: ").split()))
+
+even_count = 0
+odd_count = 0
+prime_count = 0
+
+for num in numbers:
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+    if is_prime(num):
+        prime_count += 1
+
+print("Number of even numbers:", even_count)
+print("Number of odd numbers:", odd_count)
+print("Number of prime numbers:",prime_count)
